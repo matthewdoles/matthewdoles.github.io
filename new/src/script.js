5e301df5f338a9e1f449b406d3037fecedacb4d2
Matthew Doles <matteddied@gmail.com>
2019-07-22 19:40:30 -0400

    Fixing HTML formatting, indenting, element IDs, and CSS classes

--- new/src/script.js
+++ new/src/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", () => {
-  if (this.pageYOffset > document.getElementById("top").offsetHeight) {
+  if (this.pageYOffset > document.getElementById("home").offsetHeight) {
     document.getElementById("mainNav").classList.add("nav-scrolled", "scroll-shadow");
   } else {
     document.getElementById("mainNav").classList.remove("nav-scrolled", "scroll-shadow");
