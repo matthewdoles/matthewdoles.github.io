23557151832ae327b1e67f956ee28348735b5d01
Matthew Doles <matteddied@gmail.com>
2019-07-28 22:46:55 -0400

    Updating folder structure, removing old website layout

--- new/src/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-  if (this.pageYOffset > document.getElementById("home").offsetHeight) {
-    document.getElementById("mainNav").classList.add("nav-scrolled", "scroll-shadow");
-  } else {
-    document.getElementById("mainNav").classList.remove("nav-scrolled", "scroll-shadow");
-  }
-});
